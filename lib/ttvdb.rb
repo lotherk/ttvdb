9ce342f443af3a5bada646c566571627dbabc713
Konrad Lother <konrad@corpex.de>
2014-03-14 13:10:08 +0100

    improved logging output

--- lib/ttvdb.rb
+++ lib/ttvdb.rb
@@ -6,7 +6,19 @@ require "ttvdb/client"
 
 
 module TTVDB
-  def self.logger
-    @logger ||= Logger.new(STDOUT)
+  def self.logger(output=STDOUT)
+    unless @logger
+      @logger = Logger.new(output)
+      @logger.formatter = proc { |severity, datetime, progname, msg|
+        kaller = caller[4]
+        file, ln, func  = kaller.split(":")
+        _nil, func = func.split("`")
+        func.gsub!(/[<>']/, "")
+        file = File.basename(file)
+        "#{datetime} #{severity} #{file}:#{func}:#{ln}: #{msg}\n"
+      }
+      @logger.level = Logger::WARN
+    end
+    @logger
   end
 end
